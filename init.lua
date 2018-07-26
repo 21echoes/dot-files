3fb1875a1724e47bfc46188cbbcc5fcd67127181
David Kettler <21echoes@gmail.com>
2018-07-26 13:49:28 -0700

    update Hammerspoon config’s dealing with terminal window now that I no longer use TotalTerminal

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+-- HammerSpoon config! http://www.hammerspoon.org/
+
 -- Meta
 function reloadConfig(files)
     doReload = false
@@ -6,6 +8,7 @@ function reloadConfig(files)
             doReload = true
         end
     end
+    hs.console.printStyledtext("saw FS change, going to reload: " .. tostring(doReload))
     if doReload then
         hs.reload()
     end
@@ -29,24 +32,64 @@ end
 -- Focused window movement -- Left half
 hs.hotkey.bind({"cmd", "alt"}, "Left", function()
   local win = hs.window.focusedWindow()
-  local f = dumbFrame(win:screen():frame())
-  f.w = f.w / 2
-  win:setFrame(f)
+  if win then
+    local f = dumbFrame(win:screen():frame())
+    if win:application():name() == "Terminal" then
+      f.w = f.w * (2/5)
+    else
+      f.w = f.w / 2
+    end
+    win:setFrame(f)
+  end
 end)
 
 -- Focused window movement -- Right half
 hs.hotkey.bind({"cmd", "alt"}, "Right", function()
   local win = hs.window.focusedWindow()
-  local f = dumbFrame(win:screen():frame())
-  f.x = f.x + (f.w / 2)
-  f.w = f.w / 2
-  win:setFrame(f)
+  if win then
+    local f = dumbFrame(win:screen():frame())
+    f.x = f.x + (f.w / 2)
+    f.w = f.w / 2
+    win:setFrame(f)
+  end
 end)
 
 -- Focused window movement -- Full screen
 hs.hotkey.bind({"cmd", "alt"}, "F", function()
   local win = hs.window.focusedWindow()
-  win:setFrame(win:screen():frame())
+  if win then
+    local f = dumbFrame(win:screen():frame())
+    win:setFrame(f)
+  end
+end)
+
+
+-- Terminal visor -- Opening and closing
+hs.hotkey.bind({"alt"}, "space", function()
+  local app = hs.appfinder.appFromName("Terminal")
+
+  if not app or not app:isRunning() then
+    local screens = hs.screen.allScreens()
+    local laptopFrame = screens[1]:frame()
+    local f = dumbFrame(laptopFrame)
+    f.w = f.w * (2/5)
+    placeAppAtFrame("Terminal", f, true)
+    return
+  end
+
+  if app:isHidden() then
+    app:unhide()
+    app:activate()
+  else
+    local focusedTerminalWindow = app:focusedWindow()
+    local focusedGlobalWindow = hs.window.focusedWindow()
+    local terminalIsFocused = focusedTerminalWindow == focusedGlobalWindow
+    if terminalIsFocused then
+      app:hide()
+    else
+      app:activate()
+    end
+  end
 end)
 
 
@@ -69,9 +112,21 @@ function placeAppAtFrame(appName, dumbFrame, launchIfNotRunning)
   local app = hs.appfinder.appFromName(appName)
   if not app or not app:isRunning() then
     if launchIfNotRunning then
-      hs.application.launchOrFocus(appName)
-      app = hs.appfinder.appFromName(appName)
+      local attempts = 0
+      while (not app or not app:isRunning()) and attempts < 3 do
+        hs.application.launchOrFocus(appName)
+        -- TODO: this next line never works :(
+        app = hs.appfinder.appFromName(appName)
+        hs.console.printStyledtext("launched "..hs.inspect.inspect(app).." from name "..appName)
+        attempts = attempts + 1
+      end
+      if not app or not app:isRunning() then
+        hs.console.printStyledtext("failed to launch "..appName)
+        return
+      else
+      end
     else
+      hs.console.printStyledtext("doing nothing for "..appName)
       return
     end
   end
@@ -81,79 +136,84 @@ function placeAppAtFrame(appName, dumbFrame, launchIfNotRunning)
   end
 end
 
+function sleep(n)
+  os.execute("sleep " .. tonumber(n))
+end
+
 -- Workspace -- Work
 -- Workspace -- Work -- Three Screens
 function setupWorkTripleWorkspace()
   local screens = hs.screen.allScreens()
-  if ((# screens) == 3) then
-    -- laptop
-      -- terminal
-    local terminalApp = hs.appfinder.appFromName("Terminal")
-    local terminalWindows = terminalApp:allWindows()
-    if ((# terminalWindows) <= 0) then
-      hs.eventtap.keyStroke({"alt"}, "Space")
-    end
-    local terminalWindow = terminalApp:focusedWindow()
-    local pinMenuSelector = {"Window", "Pin Visor"}
-    local pinMenuItem = terminalApp:findMenuItem(pinMenuSelector)
-    if (pinMenuItem and not pinMenuItem["ticked"]) then
-      terminalApp:selectMenuItem(pinMenuSelector)
-    end
+  -- laptop
+    -- terminal
+  local laptopFrame = screens[1]:frame()
+  local terminalFrame = dumbFrame(laptopFrame)
+  terminalFrame.w = (laptopFrame.w * (2/5))
+  placeAppAtFrame("Terminal", terminalFrame, true)
     -- other windows
-    local laptopFrame = screens[1]:frame()
-    local evernoteFrame = dumbFrame(laptopFrame)
-    local terminalFrame = dumbFrame(terminalWindow:frame())
-    evernoteFrame.x = terminalFrame.x + terminalFrame.w
-    evernoteFrame.w = laptopFrame.w - evernoteFrame.x
-    placeAppAtFrame("Evernote", evernoteFrame, true)
+  local evernoteFrame = dumbFrame(laptopFrame)
+  evernoteFrame.x = terminalFrame.x + terminalFrame.w
+  evernoteFrame.w = laptopFrame.w - evernoteFrame.x
+  placeAppAtFrame("Evernote", evernoteFrame, true)
 
-    -- monitor 1
-    local monitor1Frame = screens[2]:frame()
-    local slackFrame = dumbFrame(monitor1Frame)
-    slackFrame.w = monitor1Frame.w / 2
-    placeAppAtFrame("Slack", slackFrame, true)
+  -- monitor 1
+  local monitor1Frame = screens[2]:frame()
+  local slackFrame = dumbFrame(monitor1Frame)
+  slackFrame.w = monitor1Frame.w / 2
+  placeAppAtFrame("Slack", slackFrame, true)
 
-    for _,appName in pairs({"Sublime Text 2", "PyCharm", "Xcode"}) do
-      local ideFrame = dumbFrame(monitor1Frame)
-      ideFrame.x = monitor1Frame.x + (monitor1Frame.w / 2)
-      ideFrame.w = monitor1Frame.w / 2
-      placeAppAtFrame(appName, ideFrame, false)
-    end
+  for _,appName in pairs({"Sublime Text 2", "PyCharm", "Xcode", "Atom"}) do
+    local ideFrame = dumbFrame(monitor1Frame)
+    ideFrame.x = monitor1Frame.x + (monitor1Frame.w / 2)
+    ideFrame.w = monitor1Frame.w / 2
+    placeAppAtFrame(appName, ideFrame, false)
+  end
 
-    -- monitor 2
-    local monitor2Frame = screens[3]:frame()
-    local firefoxFrame = dumbFrame(monitor2Frame)
-    placeAppAtFrame("Firefox", firefoxFrame, true)
+  -- monitor 2
+  local monitor2Frame = screens[3]:frame()
+  local referenceFrame = dumbFrame(monitor2Frame)
+  for _,appName in pairs({"GitX", "Firefox Developer Edition"}) do
+    placeAppAtFrame(appName, referenceFrame, true)
+  end
+  for _,appName in pairs({"Google Chrome", "Safari"}) do
+    placeAppAtFrame(appName, referenceFrame, false)
   end
 end
-local workWorkspaceTripleScreenWatcher = hs.screen.watcher.new(setupWorkTripleWorkspace)
-workWorkspaceTripleScreenWatcher:start()
 
 -- Workspace -- Work -- One Screen
 function setupWorkSingleWorkspace()
   local screens = hs.screen.allScreens()
-  if ((# screens) == 1) then
-    -- terminal
-    local terminalApp = hs.appfinder.appFromName("Terminal")
-    local terminalWindows = terminalApp:allWindows()
-    if ((# terminalWindows) <= 0) then
-      hs.eventtap.keyStroke({"alt"}, "Space")
-    end
-    local terminalWindow = terminalApp:focusedWindow()
-    local pinMenuSelector = {"Window", "Pin Visor"}
-    local pinMenuItem = terminalApp:findMenuItem(pinMenuSelector)
-    if (pinMenuItem and pinMenuItem["ticked"]) then
-      terminalApp:selectMenuItem(pinMenuSelector)
-    end
-    hs.eventtap.keyStroke({"alt"}, "Space")
+  local laptopFrame = dumbFrame(screens[1]:frame())
 
-    -- other windows
-    local laptopFrame = dumbFrame(screens[1]:frame())
-    local windows = hs.window.allWindows()
-    for _,window in pairs(windows) do
-      window:setFrame(laptopFrame)
+  -- other windows
+  local windows = hs.window.allWindows()
+  for _,window in pairs(windows) do
+    window:setFrame(laptopFrame)
+  end
+
+  -- terminal
+  local terminalFrame = dumbFrame(laptopFrame)
+  terminalFrame.w = (laptopFrame.w * (2/5))
+  placeAppAtFrame("Terminal", terminalFrame, true)
+end
+
+-- Workspace -- Work -- Screen Watcher
+local prevNumScreens = 0
+function screenWatcher()
+  local screens = hs.screen.allScreens()
+  hs.console.printStyledtext("have screens " .. (# screens))
+  if ((# screens) == prevNumScreens) then
+  else
+    prevNumScreens = (# screens)
+    if ((# screens) == 3) then
+      setupWorkTripleWorkspace()
+    elseif ((# screens) == 1) then
+      setupWorkSingleWorkspace()
     end
   end
 end
-local workWorkspaceSingleScreenWatcher = hs.screen.watcher.new(setupWorkSingleWorkspace)
-workWorkspaceSingleScreenWatcher:start()
+local workspaceScreenWatcher = hs.screen.watcher.new(screenWatcher)
+workspaceScreenWatcher:start()
+screenWatcher()
+
+hs.console.printStyledtext("reloaded config")
